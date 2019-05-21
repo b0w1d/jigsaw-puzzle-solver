af208fe168d678a178f1d5ce51c087f2e3f62d96
b0w1d <61a5t0@gmail.com>
2019-05-22 01:48:48 +0800

    Working on feature detection

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <set>
 #include <queue>
 #include <algorithm>
 #include <numeric>
@@ -11,14 +12,14 @@
 #include <cmath>
 #include <cassert>
 #include "opencv2/opencv.hpp"
-#include "src/array.hpp"
+#include "src/vec2.hpp"
 #include "src/kika.hpp"
 
 void flood(
   int R, int C, // #row, #col
   int sx, int sy, int c, // start x, y, color to fill
   std::function<bool(int, int)> invalid, // (x, y).invalid?
-  Array<Array<int>> &col // to color on, initialize with 0
+  std::vector<std::vector<int>> &col // to color on, initialize with 0
 ) {
   std::queue<int> que;
   que.push(sx * C + sy);
@@ -41,7 +42,7 @@ void flood(
   }
 };
 
-Array<Array<kika::cod>> extractPieces(const cv::Mat &img) {
+std::vector<std::vector<kika::cod>> extractPieces(const cv::Mat &img) {
   cv::Mat img_inv(img.rows, img.cols, CV_8UC3);
   for (int i = 0; i < img.rows; ++i) {
     for (int j = 0; j < img.cols; ++j) {
@@ -54,20 +55,20 @@ Array<Array<kika::cod>> extractPieces(const cv::Mat &img) {
   cv::Mat img_gray;
   cv::cvtColor(img_inv, img_gray, cv::COLOR_BGR2GRAY);
 
-  Array<Array<int>> mat(img_gray.rows, Array<int>(img_gray.cols));
-  for (int i = 0; i < img_gray.rows; ++i) {
-    for (int j = 0; j < img_gray.cols; ++j) {
+  std::vector<std::vector<int>> mat(img.rows, std::vector<int>(img.cols));
+  for (int i = 0; i < img.rows; ++i) {
+    for (int j = 0; j < img.cols; ++j) {
       mat[i][j] = img_gray.at<uchar>(i, j);
     }
   }
-  Array<Array<int>> sobel_filter_x = {
+  std::vector<std::vector<int>> sobel_filter_x = {
     {-1, 0, +1},
     {-2, 0, +2},
     {-1, 0, +1}
   };
-  Array<Array<int>> sobel_filter_y = sobel_filter_x.transpose();
-  Array<Array<int>> gx = mat.convolve(sobel_filter_x);
-  Array<Array<int>> gy = mat.convolve(sobel_filter_y);
+  std::vector<std::vector<int>> sobel_filter_y = vec2::transpose(sobel_filter_x);
+  std::vector<std::vector<int>> gx = vec2::convolve(mat, sobel_filter_x);
+  std::vector<std::vector<int>> gy = vec2::convolve(mat, sobel_filter_y);
   cv::Mat res(img.rows, img.cols, CV_8U);
   for (int i = 1; i + 1 < img.rows; ++i) {
     for (int j = 1; j + 1 < img.cols; ++j) {
@@ -77,7 +78,7 @@ Array<Array<kika::cod>> extractPieces(const cv::Mat &img) {
     }
   }
   int col_cnt = 0; // assumes between every piece there is some space, as well as the corner
-  Array<Array<int>> col(res.rows, Array<int>(res.cols, -1));
+  std::vector<std::vector<int>> col(res.rows, std::vector<int>(res.cols, -1));
   flood(res.rows, res.cols, 0, 0, col_cnt,
         [&](int x, int y) { return 0 < res.at<uchar>(x, y); }, col);
   for (int i = 0; i < res.rows; ++i) {
@@ -88,7 +89,7 @@ Array<Array<kika::cod>> extractPieces(const cv::Mat &img) {
             [](int x, int y) { return false; }, col);
     }
   }
-  Array<Array<kika::cod>> col2pos(col_cnt + 1);
+  std::vector<std::vector<kika::cod>> col2pos(col_cnt + 1);
   for (int i = 0; i < res.rows; ++i) {
     for (int j = 0; j < res.cols; ++j) {
       if (col[i][j]) {
@@ -96,10 +97,10 @@ Array<Array<kika::cod>> extractPieces(const cv::Mat &img) {
       }
     }
   }
-  return col2pos.slice(1);
+  return vec2::slice(col2pos, 1);
 }
 
-void render(cv::Mat &mat, const Array<kika::cod> &pnts) {
+void render(cv::Mat &mat, const std::vector<kika::cod> &pnts) {
   for (auto pnt : pnts) {
     int x = real(pnt);
     int y = imag(pnt);
@@ -107,22 +108,24 @@ void render(cv::Mat &mat, const Array<kika::cod> &pnts) {
   }
 }
 
-void render(cv::Mat &mat, const Array<Array<int>> &col) {
+void render(cv::Mat &mat, const std::vector<std::vector<int>> &col) {
   for (int i = 0; i < col.size(); ++i) {
     for (int j = 0; j < col[0].size(); ++j) {
-      mat.at<uchar>(i, j) = col[i][j];
+      if (col[i][j]) {
+        mat.at<uchar>(i, j) = col[i][j];
+      }
     }
   }
 }
 
-Array<Array<int>> detectPieceFeatures(int R, int C, Array<kika::cod> pnts) {
-  Array<Array<int>> res(R, Array<int>(C));
+std::vector<std::vector<int>> detectPieceFeatures(int R, int C, std::vector<kika::cod> pnts) {
+  std::vector<std::vector<int>> res(R, std::vector<int>(C, -1));
   for (const kika::cod &pnt : pnts) {
-    int x = real(pnt);
-    int y = imag(pnt);
-    res[x][y] = 100;
+    int x = std::real(pnt);
+    int y = std::imag(pnt);
+    res[x][y] = 200;
   }
-  Array<kika::cod> ch = kika::convex_hull(pnts);
+  std::vector<kika::cod> ch = kika::convex_hull(pnts);
   ch.push_back(ch[0]);
   for (int i = 0; i + 1 < ch.size(); ++i) {
     int lx = real(ch[i]);
@@ -134,43 +137,82 @@ Array<Array<int>> detectPieceFeatures(int R, int C, Array<kika::cod> pnts) {
     if (std::abs(dy) < std::abs(dx)) {
       double t = 1.0 * dy / dx;
       for (int j = std::min(0, dx); j < std::max(0, dx); ++j) {
+        assert(0 <= lx + j && lx + j < R);
+        assert(0 <= ly + t * j && ly + t * j < C);
         res[lx + j][ly + t * j] = 200;
       }
     } else {
       double t = 1.0 * dx / dy;
       for (int j = std::min(0, dy); j < std::max(0, dy); ++j) {
+        assert(0 <= ly + j && ly + j < C);
+        assert(0 <= lx + t * j && lx + t * j < R);
         res[lx + t * j][ly + j] = 200;
       }
     }
   }
-  for (auto p : ch) {
-    res[std::real(p)][std::imag(p)] = 255;
+  int col_cnt = 0;
+  for (int i = 0; i < R; ++i) {
+    for (int j = 0; j < C; ++j) {
+      if (~res[i][j]) continue;
+      flood(R, C, i, j, col_cnt * 20, [](int x, int y) { return false; }, res);
+      ++col_cnt;
+    }
+  }
+  std::vector<std::vector<kika::cod>> mat(col_cnt - 1);
+  for (int i = 0; i < R; ++i) {
+    for (int j = 0; j < C; ++j) {
+      if (0 < res[i][j] && res[i][j] < col_cnt * 20) {
+        mat[res[i][j] / 20 - 1].push_back(kika::cod(i, j));
+      }
+    }
+  }
+  for (int c = 0; c < mat.size(); ++c) {
+    if (mat[c].size() < 10) continue;
+    double mx = vec2::reduce(
+      vec2::map(
+        mat[c],
+        [](kika::cod p) { return std::real(p); }
+      ),
+      std::plus<double>()
+    ) / mat[c].size();
+    double my = vec2::reduce(
+      vec2::map(
+        mat[c],
+        [](kika::cod p) { return std::imag(p); }
+      ),
+      std::plus<double>()
+    ) / mat[c].size();
+    double sx = vec2::reduce(
+      vec2::map(
+        vec2::map(
+          mat[c],
+          [&](kika::cod p) { return (std::real(p) - mx); }
+        ),
+        [](double v) { return v * v; }
+      ),
+      std::plus<double>()
+    ) / mat[c].size();
+    double sy = vec2::reduce(
+      vec2::map(
+        vec2::map(
+          mat[c],
+          [&](kika::cod p) { return (std::imag(p) - my); }
+        ),
+        [](double v) { return v * v; }
+      ),
+      std::plus<double>()
+    ) / mat[c].size();
+    for (const auto &p : mat[c]) {
+      int x = std::real(p);
+      int y = std::imag(p);
+      if (std::abs(sx - sy) / (sx + sy) < 0.2) { // concave
+        res[x][y] = 50;
+      } else { // convex
+        res[x][y] = 100;
+      }
+    }
   }
   return res;
-  /* cv::Mat res_masked(res.rows, res.cols, CV_8U); */
-  /* for (int c = 1; c < col_cnt + 1; ++c) { */
-  /*   int mx = Array<kika::cod>(col2pos[c]) */
-  /*     .map([](kika::cod c) { return std::real(c); }) */
-  /*     .reduce([](Array<double> s, double x) { */
-  /*       return Array<double>({std::min(s[0], x), std::max(s[1], x)}); */
-  /*     }, Array<double>({std::numeric_limits<double>::max(), 0})) */
-  /*     .reduce([](double x, double y) { return (x + y) / 2; }); */
-  /*   int my = Array<kika::cod>(col2pos[c]) */
-  /*     .map([](kika::cod c) { return std::imag(c); }) */
-  /*     .reduce([](Array<double> s, double x) { */
-  /*       return Array<double>({std::min(s[0], x), std::max(s[1], x)}); */
-  /*     }, Array<double>({std::numeric_limits<double>::max(), 0})) */
-  /*     .reduce([](double x, double y) { return (x + y) / 2; }); */
-  /*   for (auto p : col2pos[c]) { */
-  /*     int i = real(p); */
-  /*     int j = imag(p); */
-  /*     kika::cod xy = kika::rotate(kika::cod(i - mx, j - my), std::acos(-1) / 8); */
-  /*     int x = i;//std::real(xy) + 0.5 + mx; */
-  /*     int y = j;//std::imag(xy) + 0.5 + my; */
-  /*     res_masked.at<uchar>(x, y) = res.at<uchar>(i, j); */
-  /*   } */
-  /* } */
-  /* return res_masked; */
 }
 
 int main(int argc, const char* argv[]) {
@@ -181,15 +223,17 @@ int main(int argc, const char* argv[]) {
 
   cv::Mat img = cv::imread(argv[1]);
 
-  Array<Array<kika::cod>> pieces = extractPieces(img);
+  std::vector<std::vector<kika::cod>> pieces = extractPieces(img);
 
-  Array<Array<int>> col = detectPieceFeatures(img.rows, img.cols, pieces[0]);
-  cv::Mat img_p0(img.rows, img.cols, CV_8U, cv::Scalar(0, 0, 0));
-  render(img_p0, col);
+  cv::Mat img_d(img.rows, img.cols, CV_8U, cv::Scalar(0));
+  for (const auto &piece : pieces) {
+    std::vector<std::vector<int>> col = detectPieceFeatures(img.rows, img.cols, piece);
+    render(img_d, col);
+  }
 
-  cv::imwrite("pieces.png", img_p0);
+  cv::imwrite("pieces.png", img_d);
   cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE); 
-  cv::imshow("Display window", img_p0);  
+  cv::imshow("Display window", img_d);  
   cv::waitKey(0);
 
   return 0;
---
+++ src/vec2.hpp
@@ -0,0 +1,69 @@
+#ifndef VEC2_HPP
+#define VEC2_HPP
+
+namespace vec2 {
+  template<typename T>
+  std::vector<T> slice(const std::vector<T> &v, int s, int sz = -1) {
+    assert(s + std::max(sz, 0) <= v.size());
+    if (sz < 0) sz = v.size() - s;
+    std::vector<T> res(sz);
+    std::copy(v.begin() + s, v.begin() + s + sz, res.begin());
+    return res;
+  }
+  template<typename T>
+  std::vector<T> transpose(const std::vector<T> &v) {
+    std::vector<T> res(v[0].size(), T(v.size()));
+    for (int i = 0; i < v.size(); ++i) {
+      for (int j = 0; j < v[0].size(); ++j) {
+        res[j][i] = v[i][j];
+      }
+    }
+    return res;
+  }
+  template<typename T>
+  std::vector<T> convolve(const std::vector<T> &v, const std::vector<T> &f) {
+    assert(f.size() == f[0].size());
+    assert(f.size() & 1);
+    std::vector<T> r(v.size(), T(v[0].size()));
+    for (int i = 0; i < v.size(); ++i) {
+      for (int j = 0; j < v[0].size(); ++j) {
+        int m = f.size() / 2;
+        for (int x = -m; x < m + 1; ++x) {
+          for (int y = -m; y < m + 1; ++y) {
+            int s = i - x;
+            int t = j - y;
+            if (0 <= s && s < v.size() && 0 <= t && t < v[0].size()) {
+              r[i][j] += f[m + x][m + y] * v[s][t];
+            }
+          }
+        }
+      }
+    }
+    return r;
+  }
+  template<typename T, typename F>
+  std::vector<T> select(const std::vector<T> &v, const F &f) {
+    std::vector<T> res(v.size());
+    std::copy_if(v.begin(), v.end(), res.begin(), f);
+    return res;
+  }
+  template<typename T, typename F>
+  std::vector<typename std::result_of<F(T)>::type> map(const std::vector<T> &v, const F &f) {
+    std::vector<typename std::result_of<F(T)>::type> res(v.size());
+    std::transform(v.begin(), v.end(), res.begin(), f);
+    return res;
+  }
+  template<typename T, typename F, typename S>
+  S reduce(const std::vector<T> &v, const F &f, S s) {
+    for (const T &u : v) s = f(s, u);
+    return s;
+  }
+  template<typename T, typename F>
+  T reduce(const std::vector<T> &v, const F &f) {
+    T s = v[0];
+    for (auto it = v.begin() + 1; it != v.end(); ++it) s = f(s, *it);
+    return s;
+  }
+};
+
+#endif
